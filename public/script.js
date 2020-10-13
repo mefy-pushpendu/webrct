87b979642fa57883569cf0661aa11f16ad5fc6c7
mefy-pushpendu <pushpendu.ghosh@mefy.care>
2020-10-13 18:48:47 +0530

    chnages

--- public/script.js
+++ public/script.js
@@ -1,7 +1,7 @@
 const socket = io('http://localhost:3000')
 const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
-  host: '/',
+  host: 'localhost',
   path:'/',
   port:443,
 })
