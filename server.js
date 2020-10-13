a4b321c3eb87ae74ba5b720a14625b0aca7a6d78
mefy-pushpendu <pushpendu.ghosh@mefy.care>
2020-10-13 20:36:20 +0530

    chnages

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const server = require('http').Server(app)
 const io = require('socket.io')(server)
 const { v4: uuidV4 } = require('uuid')
 const { ExpressPeerServer } = require('peer');
-
+const cors = require('cors');
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
-
+app.use(cors());
 app.get('/', (req, res) => {
   res.redirect(`/${uuidV4()}`)
 })
