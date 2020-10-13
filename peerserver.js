5490d8892a80852644bfb346182d9f7577ef2663
mefy-pushpendu <pushpendu.ghosh@mefy.care>
2020-10-13 22:57:10 +0530

    peer server created

--- peerserver.js
+++ peerserver.js
@@ -1,4 +1,12 @@
-const { PeerServer } = require('peer');
-const peerServer = PeerServer({});
-console.log(peerServer)
+const { ExpressPeerServer } = require('peer');
+const cors = require("cors");
+const express = require("express");
+const app = express();
+app.use(cors());
+const server = app.listen(9000);
+const peerServer = ExpressPeerServer(server, {
+    path: '/myapp'
+});
+
+app.use('/peerjs', peerServer);
 
