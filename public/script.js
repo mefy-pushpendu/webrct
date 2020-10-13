b27ed6dd17e472637415a1449e92341be51d9518
mefy-pushpendu <pushpendu.ghosh@mefy.care>
2020-10-13 20:40:46 +0530

    change

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-const socket = io('https://webrtc.mefy.care', {transports: ['websocket']});
+const socket = io('https://webrtc.mefy.care', {transports: ['polling']});
 const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer({})
 const myVideo = document.createElement('video')
